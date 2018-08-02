fc9c0cd3a4a30ddc9facb0beddfb040a101b01a4
Arkiver2 <arkiver@protonmail.com>
2018-08-02 22:28:06 +0200

    Install warc instead of requests.

--- warrior-install.sh
+++ warrior-install.sh
@@ -1,7 +1,7 @@
-if ! sudo pip freeze | grep -q requests
+if ! sudo pip freeze | grep -q warc==0.2.1
 then
-  echo "Installing Requests"
-  if ! sudo pip install requests
+  echo "Installing warc 0.2.1"
+  if ! sudo pip install warc --upgrade
   then
     exit 1
   fi
